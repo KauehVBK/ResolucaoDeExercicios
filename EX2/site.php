dded96b34279b467cab5fe0591ed0b95824b308b
kauehvbk <kauehvbk@gmail.com>
2022-08-27 13:24:25 -0300

    primeiro commit

---
+++ EX2/site.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Docasd</title>
+</head>
+
+<body>
+
+    <form action="/phpw/www.php" method="POST">
+    <label>data 1<br></label>
+    
+    <input type="date" name="data"><br>
+    
+    <label>data 2<br></label>
+    
+    <input type="date" name="data1"><br>
+    <hr>
+    <input type="submit" value="enviar">
+
+    </form>
+
+    <?php
+
+
+
+    ?>
+
+</body>
+
+</html>
---
+++ EX2/www.php
@@ -0,0 +1,47 @@
+
+<?php
+include_once('site.php');
+
+$datans = array(0, 1);
+$datans[0] = $_POST["data"];
+$datans[1] = $_POST["data1"];
+$idade = array(0, 1);
+
+    
+$data = $datans[0];
+list($ano, $mes, $dia) = explode('-', $data);
+$hoje = mktime(0, 0, 0, date('m'), date('d'), date('Y'));
+$nascimento = mktime( 0, 0, 0, $mes, $dia, $ano);
+$idade[0] = floor((((($hoje - $nascimento) / 60) / 60) / 24) / 365.25);
+echo $idade[0];
+echo"<br>";
+
+$data2 = $datans[1];
+list($ano2, $mes, $dia) = explode('-', $data2);
+$hoje = mktime(0, 0, 0, date('m'), date('d'), date('Y'));
+$nascimento = mktime( 0, 0, 0, $mes, $dia, $ano2);
+$idade[1] = floor((((($hoje - $nascimento) / 60) / 60) / 24) / 365.25);
+echo $idade[1];
+
+
+
+if ($idade[0]<$idade[1]){
+
+   
+ $idadeve = ($idade[1])/2;
+  echo"<br> $idadeve";
+  $idadef = $idadeve+$ano;
+  echo"<br> $idadef";  
+
+}else{
+    $idadeve = ($idade[0])/2;
+    echo"<br> $idadeve";
+    $idadef = $idadeve+$ano2;
+    echo"<br> $idadef";
+}
+
+
+?>
+
+
+
