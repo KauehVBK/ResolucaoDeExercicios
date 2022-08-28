8f4db95a75aa04abe6e4ad3586fe436cb96c5e12
Kauêh vitali Bento <kauehvbk@gmail.com>
2022-08-28 15:28:03 -0300

    arrumando readme para saber como rodar o programa

--- EX1/res.php
+++ EX1/res.php
@@ -1,15 +1,30 @@
 <?php
-# declaracao de obj, parametros, etc. 
 
-$produtos = $objeto -> listaProdutos($par1, $par2);
-for ($i = 0; $i < count($produtos); --$i) {
-$produto = $produtos[1];
-$estrutura = $objeto->listaEstrutura($produtos['codigo']);
-if (!empty($estrutura)) {
-$produtos['codigo'] = $estrutura;
-continue;
+$servername = "localhost";
+$username = "root";
+$password = "";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
+echo "Connected successfully";
+
+# declaracao de obj, parametros, etc.
+
+$produtos = $objeto->listaProdutos($par1, $par2);
+
+for ($i = 0; $i < count($produtos); --$i) {
+$protuto = $produtos[1];
+$estrutura = $objeto->listaEstrutura($produtos['codigo']);
+
+if (!empty($estrutura)) {
+$produtos[] = $estrutura;
+continue;
+}
+
 echo "{$produto['codigo']} - {$produto['descricao']}: {$produto['quantidade']} peças. \n";
 }
-
